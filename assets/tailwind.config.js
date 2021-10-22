e49554a3d3b98591b0200d38548cf14a0b5185b4
Antti Mattila <antti.mattila@futurice.com>
2021-10-22 19:41:47 +0300

    Add Tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.eex", "../lib/*_web/**/*.heex"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
