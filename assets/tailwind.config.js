4482343de62fe4fce6f179da2edd836e96a9bb98
Antti Mattila <me@rarelyneeded.com>
2022-02-06 08:37:57 +0200

    Update Tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   mode: "jit",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.eex", "../lib/*_web/**/*.heex", "../lib/*_web/**/*_component.ex"],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./js/**/*.js",
+    "../lib/*_web/**/*.eex",
+    "../lib/*_web/**/*.heex",
+    "../lib/*_web/**/*_component.ex",
+  ],
+  darkMode: "media",
   theme: {
     extend: {},
   },
