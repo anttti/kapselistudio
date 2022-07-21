daa16222cf39b448e6d074a679479eddc73ba6e6
Antti Mattila <me@rarelyneeded.com>
2022-07-21 18:34:32 +0300

    Remove unused code

--- lib/kapselistudio_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule KapselistudioWeb.PageController do
-  use KapselistudioWeb, :controller
-
-  def index(conn, _params) do
-    text(conn, "Subdomain home for #{conn.private[:subdomain]}")
-  end
-end
