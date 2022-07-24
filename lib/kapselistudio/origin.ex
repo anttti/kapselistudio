59859c01e9b70cb8ae422c4e3aff573455785c0c
Antti Mattila <me@rarelyneeded.com>
2022-07-24 13:02:17 +0300

    Add debug logging

--- lib/kapselistudio/origin.ex
+++ lib/kapselistudio/origin.ex
@@ -5,6 +5,11 @@ defmodule Kapselistudio.Origin do
 
   def get_subdomain(host) do
     root_host = KapselistudioWeb.Endpoint.config(:url)[:host]
+
+    IO.inspect(
+      "Getting subdomain, root_host: #{root_host}, parsed: #{String.replace(host, ~r/.?#{root_host}/, "")}"
+    )
+
     String.replace(host, ~r/.?#{root_host}/, "") |> String.replace(":4000", "")
   end
 end
