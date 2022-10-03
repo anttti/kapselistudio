04459ee179adf2fe41f2a5d7cb23a53fae11b32f
Antti Mattila <me@rarelyneeded.com>
2022-10-03 14:13:44 +0300

    Adjust headings

--- lib/kapselistudio_web/live/website_live/show_podcast.ex
+++ lib/kapselistudio_web/live/website_live/show_podcast.ex
@@ -40,7 +40,6 @@ defmodule KapselistudioWeb.WebsiteLive.ShowPodcast do
     <.page name={@podcast.name} description={@podcast.description} author={@podcast.author}>
       <section class="flex flex-col gap-4 md:gap-8 p-4 md:p-8">
         <div class="flex flex-col gap-2 md:gap-4 p-4 md:p-8 bg-primary text-white rounded-xl max-w-4xl mx-auto">
-          <h2 class="uppercase font-bold text-sm tracking-widest">Uusin jakso</h2>
           <div class="flex gap-4 md:gap-8">
             <div class="text-4xl md:text-6xl font-extrabold mt-[-2px] text-primary-dark">
               <%= @latest_episode.number %>
@@ -74,7 +73,9 @@ defmodule KapselistudioWeb.WebsiteLive.ShowPodcast do
         <div class="max-w-4xl mx-auto px-8">
           <div class="flex items-center">
             <hr class="h-[1px] bg-gray-200 flex-1" />
-            <h2 class="px-4 uppercase font-bold text-sm tracking-widest">Aiemmat jaksot</h2>
+            <h2 class="px-4 uppercase font-bold text-sm tracking-widest text-gray-400">
+              Aiemmat jaksot
+            </h2>
             <hr class="h-[1px] bg-gray-200 flex-1" />
           </div>
           <ol class="divide-y divide-gray-200">
